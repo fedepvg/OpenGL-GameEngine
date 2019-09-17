4250dea7b10da58c8123e708c36f259d24483a70
fedepvg <fedricopvg@gmail.com>
2019-09-17 09:25:53 -0300

    .bat added

---
+++ GameEngine/Engine/copy-dll.bat
@@ -0,0 +1,11 @@
+  
+@echo off
+
+set solutiondir=%1
+set target=%2
+
+echo Copiando archivos...
+
+xcopy "%solutiondir%libs\glew-2.1.0\bin\Release\Win32\glew32.dll" "%target%" /E /Y /S
+
+echo Copia finalizada
