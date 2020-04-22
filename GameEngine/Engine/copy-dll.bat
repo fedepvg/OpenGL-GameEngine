e1660e824bf70d03b1cada75138a9a6d75a56a51
pancreasio <julianholisu@gmail.com>
2020-04-22 13:25:10 -0300

    Update copy-dll.bat

--- GameEngine/Engine/copy-dll.bat
+++ GameEngine/Engine/copy-dll.bat
@@ -6,7 +6,12 @@ set target=%2
 
 echo Copiando archivos...
 
-xcopy "%solutiondir%libs\glew-2.1.0\bin\Release\Win32\glew32.dll" "%target%" /E /Y /S
-xcopy "%solutiondir%libs\assimp\lib\assimp-vc141-mt.dll" "%target%" /E /Y /S
+xcopy %solutiondir%libs\assimp\lib\assimp-vc141-mt.dll %target% /y /s
+
+xcopy %solutiondir%libs\glew-2.1.0\bin\Release\Win32\glew32.dll %target% /y /s
+echo Copied Glew32.dll
+
+
+echo Copied Assimp-vc141-mt.dll
 
 echo Copia finalizada
