cf7696e77cce367109424d4a8d41af32878e6991
pancreasio <julianholisu@gmail.com>
2020-05-20 20:16:40 -0300

    green texted all 2D functionalities in Update() of Game.cpp

--- GameEngine/src/Game/Game/Game.cpp
+++ GameEngine/src/Game/Game/Game.cpp
@@ -27,11 +27,11 @@ void Game::Init()
 	Texture comuTex("../res/texture.png");
 	Texture animTex("../res/anim.png");
 
-	shape = new Shape({ 0,0,0 }, { 100,100,0 }, &bokeTex);
+	/*shape = new Shape({ 0,0,0 }, { 100,100,0 }, &bokeTex);
 	shape2 = new Shape({ 30.f,30.f,0 }, { 100,100,0 }, &comuTex);
 	anim = new Sprite({ 0,0,0 }, { 368, 200, 0 }, &animTex);
 	anim->CreateAnimation(368, 368 / 8, 4);
-	anim->SetCurrentAnimation(8, 0, 2);
+	anim->SetCurrentAnimation(8, 0, 2);*/
 	ourShader = new Shader("../src/Engine/3DVertexShader.vertexshader", "../src/Engine/3DFragmentShader.fragmentshader");
 	testModel = new Model("../res/model/backpack.obj", ourShader);
 	safePositionExists = false;
@@ -43,98 +43,100 @@ void Game::Init()
 
 void Game::Update(const float deltaTime)
 {
+
+	//scaling
+	//anim->UpdateCurrentAnimation(deltaTime);
+
+	//if (input->GetKey(GLFW_KEY_H))
+	//{
+	//	anim->Scale({ 1.1f,1.1f,1.1f });
+	//}
+
+	//if (input->GetKey(GLFW_KEY_K))
+	//{
+	//	anim->Scale({ 0.9f,0.9f,0.9f });
+	//}
+
+	//if (input->GetKey(GLFW_KEY_N))
+	//{
+	//	shape2->Scale({ 1.1f,1.1f,1.1f });
+	//}
+
+	//if (input->GetKey(GLFW_KEY_M))
+	//{
+	//	shape2->Scale({ 0.9f,0.9f,0.9f });
+	//}
+
+	////rotating
+	//if (input->GetKey(GLFW_KEY_E))
+	//{
+	//	anim->Rotate(-1.0f, { 0.0f,0.0f,1.f });
+	//	anim->SetCurrentAnimation(8, 2, 2);
+	//}
+
+	//if (input->GetKey(GLFW_KEY_F))
+	//{
+	//	anim->Rotate(-1.0f, { 0.0f,1.0f,0.f });
+	//	anim->SetCurrentAnimation(8, 2, 2);
+	//}
+
+	//else if (input->GetKey(GLFW_KEY_Q))
+	//{
+	//	anim->Rotate(1.0f, { 0.0f,0.0f,1.f });
+	//	anim->SetCurrentAnimation(6, 1, 2);
+	//}
+
+	//translating
+
+	//if (input->GetKey(GLFW_KEY_LEFT))
+	//{
+	//	anim->Translate(-60.f, { 1.0f,0.0f,0.0f });
+	//	anim->SetCurrentAnimation(8, 0, 2);
+	//}
+
+	//if (input->GetKey(GLFW_KEY_RIGHT))
+	//{
+	//	anim->Translate(60.f, { 1.0f,0.0f,0.0f });
+	//	anim->SetCurrentAnimation(8, 0, 2);
+	//}
+
+	//if (input->GetKey(GLFW_KEY_UP))
+	//{
+	//	anim->Translate(60.f, { 0.0f,1.0f,0.0f });
+	//	anim->SetCurrentAnimation(8, 0, 2);
+	//}
+	//if (input->GetKey(GLFW_KEY_DOWN))
+	//{
+	//	anim->Translate(-60.f, { 0.0f,1.0f,0.0f });
+	//	anim->SetCurrentAnimation(8, 0, 2);
+	//}
+
+	////collisions
+
+	//if (collisionManager->CheckCollision(*shape, *shape2))
+	//{
+	//	if (safePositionExists)
+	//		shape2->SetPosition(safePosition);
+	//}
+	//else
+	//{
+	//	safePosition = shape2->GetPosition();
+	//	safePositionExists = true;
+	//}
+
+	//if (input->GetKey(GLFW_KEY_SPACE))
+	//{
+	//	shape2->SetPosition({ 30.f,30.f });
+	//}
+
 	if (cursor->GetCursorMode() != Cursor::CursorMode::capture)
 		cursor->SetCursorMode(Cursor::CursorMode::capture);
 
-	//scaling
-	anim->UpdateCurrentAnimation(deltaTime);
-
-	if (input->GetKey(GLFW_KEY_H))
-	{
-		anim->Scale({ 1.1f,1.1f,1.1f });
-	}
-
-	if (input->GetKey(GLFW_KEY_K))
-	{
-		anim->Scale({ 0.9f,0.9f,0.9f });
-	}
-
-	if (input->GetKey(GLFW_KEY_N))
-	{
-		shape2->Scale({ 1.1f,1.1f,1.1f });
-	}
-
-	if (input->GetKey(GLFW_KEY_M))
-	{
-		shape2->Scale({ 0.9f,0.9f,0.9f });
-	}
-
-	//rotating
-	if (input->GetKey(GLFW_KEY_E))
-	{
-		anim->Rotate(-1.0f, { 0.0f,0.0f,1.f });
-		anim->SetCurrentAnimation(8, 2, 2);
-	}
-
-	if (input->GetKey(GLFW_KEY_F))
-	{
-		anim->Rotate(-1.0f, { 0.0f,1.0f,0.f });
-		anim->SetCurrentAnimation(8, 2, 2);
-	}
-
-	else if (input->GetKey(GLFW_KEY_Q))
-	{
-		anim->Rotate(1.0f, { 0.0f,0.0f,1.f });
-		anim->SetCurrentAnimation(6, 1, 2);
-	}
-
-	//translating
 	if (input->GetKey(GLFW_KEY_X))
 	{
 		renderCamera->RotatePitch(10.f);
 	}
-
-	if (input->GetKey(GLFW_KEY_LEFT))
-	{
-		anim->Translate(-60.f, { 1.0f,0.0f,0.0f });
-		anim->SetCurrentAnimation(8, 0, 2);
-	}
-
-	if (input->GetKey(GLFW_KEY_RIGHT))
-	{
-		anim->Translate(60.f, { 1.0f,0.0f,0.0f });
-		anim->SetCurrentAnimation(8, 0, 2);
-	}
-
-	if (input->GetKey(GLFW_KEY_UP))
-	{
-		anim->Translate(60.f, { 0.0f,1.0f,0.0f });
-		anim->SetCurrentAnimation(8, 0, 2);
-	}
-	if (input->GetKey(GLFW_KEY_DOWN))
-	{
-		anim->Translate(-60.f, { 0.0f,1.0f,0.0f });
-		anim->SetCurrentAnimation(8, 0, 2);
-	}
-
-	//collisions
-
-	if (collisionManager->CheckCollision(*shape, *shape2))
-	{
-		if (safePositionExists)
-			shape2->SetPosition(safePosition);
-	}
-	else
-	{
-		safePosition = shape2->GetPosition();
-		safePositionExists = true;
-	}
-
-	if (input->GetKey(GLFW_KEY_SPACE))
-	{
-		shape2->SetPosition({ 30.f,30.f });
-	}
-
+	
 	testModel->Draw();
 
 	//camera
