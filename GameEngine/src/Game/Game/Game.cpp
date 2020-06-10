e673c1fdb375c3b89fb93b5fb057243560cd66ab
pancreasio <julianholisu@gmail.com>
2020-06-10 20:49:03 -0300

    deleted 2D functionality and greentext from Game.cpp

--- GameEngine/src/Game/Game/Game.cpp
+++ GameEngine/src/Game/Game/Game.cpp
@@ -15,27 +15,16 @@ Game::Game(int width, int height) : BaseGame(width, height)
 
 Game::~Game()
 {
-	delete shape;
-	delete shape2;
-	delete anim;
 	delete cursor;
 	delete ourShader;
+	delete testModel;
+	delete dirLight;
 }
 
 void Game::Init()
 {
-	Texture bokeTex("../res/BOKEE.png");
-	Texture comuTex("../res/texture.png");
-	Texture animTex("../res/anim.png");
-
-	/*shape = new Shape({ 0,0,0 }, { 100,100,0 }, &bokeTex);
-	shape2 = new Shape({ 30.f,30.f,0 }, { 100,100,0 }, &comuTex);
-	anim = new Sprite({ 0,0,0 }, { 368, 200, 0 }, &animTex);
-	anim->CreateAnimation(368, 368 / 8, 4);
-	anim->SetCurrentAnimation(8, 0, 2);*/
 	ourShader = new Shader("../src/Engine/3DVertexShader.vertexshader", "../src/Engine/3DFragmentShader.fragmentshader");
-	safePositionExists = false;
-	safePosition = { 0.f,0.f };
+
 	fpsCamera = new FirstPersonCameraController(renderCamera, cursor);
 	fpsCamera->SetSensitivity(0.3f);
 	//shader initialization
@@ -52,91 +41,6 @@ void Game::Init()
 void Game::Update(const float deltaTime)
 {
 	ourShader->PassDirectionalLightValues(dirLight);
-	
-	//scaling
-	//anim->UpdateCurrentAnimation(deltaTime);
-
-	//if (input->GetKey(GLFW_KEY_H))
-	//{
-	//	anim->Scale({ 1.1f,1.1f,1.1f });
-	//}
-
-	//if (input->GetKey(GLFW_KEY_K))
-	//{
-	//	anim->Scale({ 0.9f,0.9f,0.9f });
-	//}
-
-	//if (input->GetKey(GLFW_KEY_N))
-	//{
-	//	shape2->Scale({ 1.1f,1.1f,1.1f });
-	//}
-
-	//if (input->GetKey(GLFW_KEY_M))
-	//{
-	//	shape2->Scale({ 0.9f,0.9f,0.9f });
-	//}
-
-	////rotating
-	//if (input->GetKey(GLFW_KEY_E))
-	//{
-	//	anim->Rotate(-1.0f, { 0.0f,0.0f,1.f });
-	//	anim->SetCurrentAnimation(8, 2, 2);
-	//}
-
-	//if (input->GetKey(GLFW_KEY_F))
-	//{
-	//	anim->Rotate(-1.0f, { 0.0f,1.0f,0.f });
-	//	anim->SetCurrentAnimation(8, 2, 2);
-	//}
-
-	//else if (input->GetKey(GLFW_KEY_Q))
-	//{
-	//	anim->Rotate(1.0f, { 0.0f,0.0f,1.f });
-	//	anim->SetCurrentAnimation(6, 1, 2);
-	//}
-
-	//translating
-
-	//if (input->GetKey(GLFW_KEY_LEFT))
-	//{
-	//	anim->Translate(-60.f, { 1.0f,0.0f,0.0f });
-	//	anim->SetCurrentAnimation(8, 0, 2);
-	//}
-
-	//if (input->GetKey(GLFW_KEY_RIGHT))
-	//{
-	//	anim->Translate(60.f, { 1.0f,0.0f,0.0f });
-	//	anim->SetCurrentAnimation(8, 0, 2);
-	//}
-
-	//if (input->GetKey(GLFW_KEY_UP))
-	//{
-	//	anim->Translate(60.f, { 0.0f,1.0f,0.0f });
-	//	anim->SetCurrentAnimation(8, 0, 2);
-	//}
-	//if (input->GetKey(GLFW_KEY_DOWN))
-	//{
-	//	anim->Translate(-60.f, { 0.0f,1.0f,0.0f });
-	//	anim->SetCurrentAnimation(8, 0, 2);
-	//}
-
-	////collisions
-
-	//if (collisionManager->CheckCollision(*shape, *shape2))
-	//{
-	//	if (safePositionExists)
-	//		shape2->SetPosition(safePosition);
-	//}
-	//else
-	//{
-	//	safePosition = shape2->GetPosition();
-	//	safePositionExists = true;
-	//}
-
-	//if (input->GetKey(GLFW_KEY_SPACE))
-	//{
-	//	shape2->SetPosition({ 30.f,30.f });
-	//}
 
 	if (cursor->GetCursorMode() != Cursor::CursorMode::capture)
 		cursor->SetCursorMode(Cursor::CursorMode::capture);
--- GameEngine/src/Game/Game/Game.h
+++ GameEngine/src/Game/Game/Game.h
@@ -2,8 +2,6 @@
 #define GAME_H
 
 #include "Engine/BaseGame.h"
-#include "Engine/Shape.h"
-#include "Engine/Sprite.h"
 #include "Engine/Model.h"
 #include "FirstPersonCameraController.h"
 
@@ -19,13 +17,8 @@ public:
 private:
 	FirstPersonCameraController* fpsCamera;
 	Shader* ourShader;
-	Shape* shape;
-	Shape* shape2;
-	Sprite* anim;
 	Model* testModel;
 	DirectionalLight* dirLight;
-	glm::vec2 safePosition;
-	bool safePositionExists;
 };
 
 #endif
